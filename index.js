8bd2d62fc01de01489e410eab32f031e6bd5e862
kbhani <kbhani@miu.edu>
2023-02-19 17:13:56 +0530

    till basic JSX

---
+++ index.js
@@ -0,0 +1,15 @@
+const navbar = (
+    <nav>
+        <h1 className="header">website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(
+    navbar,
+    document.getElementById("root")
+);
